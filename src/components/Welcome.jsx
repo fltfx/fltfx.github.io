ec115635a629360ff3737ee206ea83a05b2fc41b
CYW <cyw@CYWs-MacBook-Pro.local>
2022-01-08 14:13:22 -0700

    added resume link to Welcome section

--- src/components/Welcome.jsx
+++ src/components/Welcome.jsx
@@ -26,6 +26,7 @@ export default function Welcome() {
           <EmailIcon />
         </IconButton>
         <p>claudiaywong@gmail.com</p>
+        <a href="https://resume.creddle.io/resume/1rvn7zz1bda">Full resume available here</a>
       </div>
     </div>
 
